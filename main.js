ab1083e2864c6d8ac3b67ec9be646dce862ed772
Zarow™ <102884028+Zarow-01@users.noreply.github.com>
2022-08-25 23:29:15 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+var RPC = require("discord-rpc")
+
+const config = require("./config.json");
+const client = new RPC.Client({ transport: 'ipc' })
+
+client.on('ready', () => {
+	client.request('SET_ACTIVITY', {
+		pid: process.pid,
+		activity: {
+			details: config.line1,
+			state: config.line2,
+			assets: {
+				large_image: config.l_img,
+				// small_image : config.s_img,
+				// small_text : config.txt_si,
+				large_text: config.txt_li
+			},
+			buttons: [
+				{ label: config.button_name1, url: config.button_url1 },
+				{ label: config.button_name2, url: config.button_url2 },
+			]
+		}
+	})
+})
+
+client.login({ clientId: config.app_id }).catch(console.error);
+console.log('Loading.')
+setTimeout(function () {
+	console.log('Loading..')
+}, 1000)
+
+setTimeout(function () {
+	console.log('Loading...')
+}, 2000)
+
+setTimeout(function () {
+	console.log('Loading....')
+}, 3000)
+
+setTimeout(function () {
+	console.log('Loading....')
+}, 4000)
+
+setTimeout(function () {
+	console.log('Loading.....')
+}, 5000)
+
+setTimeout(function () {
+	console.log('RPC Good ! By Zarow™#3936')
+}, 6000)
