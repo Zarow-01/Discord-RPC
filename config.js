8353ea970a28ff329a3a44d97e52deed8e57f010
Zarow <102884028+Zarow-01@users.noreply.github.com>
2023-04-02 00:17:28 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     app_id: '1091789525791035412',
 
-    line1: 'Zarow\'RPC Project.',
+    line1: 'RPC Project.',
     line2: 'By Zarow#3936.',
 
     txt_li: 'Made by Zarow#3936!',
@@ -10,8 +10,8 @@ module.exports = {
     l_img: 'picture_1',
     s_img: 'picture_2',
 
-    button_name1: 'Discord',
-    button_name2: 'GitHub',
+    button_name1: 'Discord.',
+    button_name2: 'GitHub.',
 
     button_url1: 'https://discord.com/users/372771862960275456',
     button_url2: 'https://github.com/Zarow-01/'
